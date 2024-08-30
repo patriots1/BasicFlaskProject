e58f373c83d46b21f9a10d8c99783cb253fe5033
Sathvik Charugundla <vikcharugun@gmail.com>
2024-08-30 12:25:25 -0400

    first stuff

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Welcome<h1>'
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8001))
+    app.run(debug=True, host='0.0.0.0', port=port)
