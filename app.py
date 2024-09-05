8ee1857a5e07912553cd67bc7d4d3970c1fc7b62
Sathvik Charugundla <vikcharugun@gmail.com>
2024-09-04 20:49:22 -0400

    created post.html - need to set up heroku account

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ def about():
     name = '30 days of Python programming'
     return render_template('about.html', name = name, title = 'About Us')
 
+@app.route('/post')
+def post():
+    name = 'Text Analyzer'
+    return render_template('post.html', name = name, title = name)
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8001))
     app.run(debug=True, host='0.0.0.0', port=port)
