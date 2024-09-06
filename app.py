e0304e68c6d366b8592b2c3e32cb612962b52292
Sathvik Charugundla <vikcharugun@gmail.com>
2024-09-05 21:43:31 -0400

    update app.py, make result route, populate css file

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 
 app = Flask(__name__)
 
+app.config['SET_FILE_MAX_AGE_DEFAULT'] = 0
+
 @app.route('/')
 def home():
     techs = ['HTML', 'CSS', 'Python', 'Flask']
@@ -14,10 +16,19 @@ def about():
     name = '30 days of Python programming'
     return render_template('about.html', name = name, title = 'About Us')
 
-@app.route('/post')
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/post', methods = ['GET', 'POST'])
 def post():
     name = 'Text Analyzer'
-    return render_template('post.html', name = name, title = name)
+    if request.method == 'GET':
+        return render_template('post.html', name = name, title = name)
+    if request.method == 'POST':
+        content = request.form['content']
+        print(content)
+        return redirect(url_for('result'))
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8001))
